94a7e6c7d327b6fbcd0c99cfe446ae340f774946
evertonlaf <evertonlaf@gmail.com>
2022-08-02 17:12:30 -0300

    add README

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -15,8 +17,7 @@ public class Main {
         c2.setDescricao("Curso de JPA ");
         c2.setCargaHoraria(12);
 
-        System.out.println(c);
-        System.out.println(c2);
+
 
 
         Mentoria m = new Mentoria();
@@ -25,9 +26,42 @@ public class Main {
         m.setData(LocalDate.now());
 
         System.out.println(m);
+        System.out.println(c);
+        System.out.println(c2);
 
 
+        Bootcamp b = new Bootcamp();
+        b.setNome("Bootcamp Java Develop");
+        b.setDescricao("Descrição Bootcamp Java Develop");
+        b.getConteudos().add(c);
+        b.getConteudos().add(c2);
+        b.getConteudos().add(m);
 
+
+        Dev dev = new Dev();
+        dev.setNome("Everton");
+        dev.inscreverBootcamp(b);
+        System.out.println("Everton-Atividades pendente-> "+ dev.getConteudosInscritos());
+        System.out.println("Everton-Atividades concluida-> "+ dev.getConteudosConcluidos());
+        System.out.println("Everton-XP-> "+ dev.calcularXP());
+        dev.progredir();
+        System.out.println("Everton-Atividades pendente-> "+ dev.getConteudosInscritos());
+        System.out.println("Everton-Atividades concluida-> "+ dev.getConteudosConcluidos());
+        System.out.println("Everton-XP-> "+ dev.calcularXP());
+
+        System.out.println("--------------------------------------------");
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Antonio");
+        dev2.inscreverBootcamp(b);
+        System.out.println("Antonio-Atividades pendente-> "+ dev2.getConteudosInscritos());
+        System.out.println("Antonio-Atividades concluida-> "+ dev2.getConteudosConcluidos());
+        System.out.println("Antonio-XP-> "+ dev2.calcularXP());
+        dev2.progredir();
+        dev2.progredir();
+        System.out.println("Antonio-Atividades pendente-> "+ dev2.getConteudosInscritos());
+        System.out.println("Antonio-Atividades concluida-> "+ dev2.getConteudosConcluidos());
+        System.out.println("Antonio-XP-> "+ dev2.calcularXP());
     }
 
 }
